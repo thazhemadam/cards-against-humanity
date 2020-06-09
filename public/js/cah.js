70c3d748c47a156035395f9fb96074b4716cabb2
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2020-06-09 16:15:27 +0530

    Copy Room Link to Clipboard
    -   The Copy Link button that is seen once the user joins a room has
        been configured to copy the room ID that a user would require to
        join the room.
    
    -   For this purpose, the jQuery library is imported and used.

--- public/js/cah.js
+++ public/js/cah.js
@@ -2,6 +2,16 @@ const socket = io();
 const {name, id}=Qs.parse(location.search, {ignoreQueryPrefix: true});
 console.log(Qs.parse(location.search, {ignoreQueryPrefix: true}));
 
+//Copy the room id to clipboard when the "Copy Link button is clicked."
+const $copylink = document.getElementById('copy-link');
+$copylink.addEventListener("click", () => {
+    let $temp = $("<input>");
+    $("#room-details").append($temp);
+    $temp.val(id).select();
+    document.execCommand("copy");
+    $temp.remove();
+})
+
 socket.emit('create', {name, id}, (err)=>{
     if(err){
         alert(err);
